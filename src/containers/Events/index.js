e123a805d2b1c142d1467cf4d4bb91be97e93892
3MM4NU3L98 <emmanuelsegeard@gmail.com>
2024-06-06 08:03:00 +0200

    EventList commenter

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -7,12 +7,20 @@ import ModalEvent from "../ModalEvent";
 
 import "./style.css";
 
+// variable indiquant le nombre de prestation par page
 const PER_PAGE = 9;
 
 const EventList = () => {
+  // récupération des données data{events: Array(18), focus: Array(3)} et error
   const { data, error } = useData();
+
+  // variable du type de prestation sélectionné
   const [type, setType] = useState();
+
+  // variable de la page en cours
   const [currentPage, setCurrentPage] = useState(1);
+
+  console.log(type)
   const filteredEvents = (
     (!type
       ? data?.events
@@ -26,13 +34,19 @@ const EventList = () => {
     }
     return false;
   });
+
+  // à chaque changement de type, on l'enregistre et eéinitialise la page courante
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
   };
-  console.log(type)
+
+  // Nombre total de pages
   const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
+
+  // Liste des types d'événements
   const typeList = new Set(data?.events.map((event) => event.type));
+
   return (
     <>
       {error && <div>An error occured</div>}
